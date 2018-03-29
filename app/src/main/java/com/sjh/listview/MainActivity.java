255b34522ec78a2dc4ca70227b5dcf38a3160474
love-Leslie-Cheung <love-Leslie-Cheung@users.noreply.github.com>
2018-03-29 14:09:08 +0800

    item内部控件的点击事件

---
+++ app/src/main/java/com/sjh/listview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.sjh.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private List<String> mList = new ArrayList<>();
+    private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview);
+        for (int i = 0; i < 20; i++) {
+            mList.add(i+"  author:Leslie___Cheung");
+        }
+        adapter = new MyAdapter(MainActivity.this, mList);
+        listView.setAdapter(adapter);
+        //ListView item的点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Click item" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //ListView item 中的删除按钮的点击事件
+        adapter.setOnItemDeleteClickListener(new MyAdapter.onItemDeleteListener() {
+            @Override
+            public void onDeleteClick(int i) {
+                mList.remove(i);
+                Toast.makeText(MainActivity.this, "delete item:" + i, Toast.LENGTH_SHORT).show();
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
